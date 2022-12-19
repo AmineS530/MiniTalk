1553ad14eaf72d8b812795fafd1b514279e33d07
Amine Sadik <asadik@e1r8p12.1337.ma>
2022-12-19 16:24:23 +0100

    testing

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: asadik <asadik@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 15:27:02 by asadik            #+#    #+#             */
-/*   Updated: 2022/12/17 15:18:30 by asadik           ###   ########.fr       */
+/*   Updated: 2022/12/19 16:14:19 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv;
 	if (argc == 3)
 	{
-		ft_printf("success");
+		if (ft_atoi(argv[1]) <= 0)
+			return (2);
+		ft_printf("Characters Sent By The Client: %d \n", ft_strlen(argv[2]));
 	}
 	else
 	{
 		ft_printf("Usage: ./client [PID] [String to send]");
+		return (1);
 	}
-	return 0;
+	return (0);
 }
