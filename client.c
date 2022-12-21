f11f326cfea12c5e4a589122d2bcfcaa2fb33748
Amine Sadik <asadik@e1r7p2.1337.ma>
2022-12-21 17:35:57 +0100

    stuff

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: asadik <asadik@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 15:27:02 by asadik            #+#    #+#             */
-/*   Updated: 2022/12/20 15:18:20 by asadik           ###   ########.fr       */
+/*   Updated: 2022/12/21 15:37:23 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,28 @@ static void	signal_send(int sign)
 		i++;
 }
 
+static void	signal_handler(int s_pid, char *str)
+{
+	int		i;
+	int		j;
+	char	owo;
+
+	i = 0;
+	while (str[i])
+	{
+		j = 8;
+		owo = str[i++];
+		while (j)
+		{
+			if (owo >> j & 1)
+				kill(s_pid, SIGUSR2);
+			else
+				kill(s_pid, SIGUSR1);
+			j--;
+		}
+	}
+}
+
 int	main(int argc, char *argv[])
 {
 	if (argc == 3)
@@ -27,6 +49,9 @@ int	main(int argc, char *argv[])
 		if (ft_atoi(argv[1]) <= 0)
 			return (2);
 		ft_printf("Characters Sent By The Client: %d \n", ft_strlen(argv[2]));
+		signal(SIGUSR1, signal_send);
+		signal(SIGUSR2, signal_send);
+		signal_handler(ft_atoi(argv[1]),argv[2]);
 	}
 	else
 	{
